1cac3f8ba863672030a5ed1561dff2351a059868
gunish-dt <gunish@devtron.ai>
2022-03-12 13:30:53 +0530

    added cors headers

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const db = require('./db')
-const movieRouter = require('./routes/movie-router')
+const db = require("./db");
+const movieRouter = require("./routes/movie-router");
 
-const app = express()
-const apiPort = 5000
+const app = express();
+const apiPort = 5000;
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+//Adding Headers
+app.options("*", cors()); // include before other routes
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, OPTIONS, HEAD");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "access-control-allow-origin,DNT,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization,token"
+  );
+  res.header("Access-Control-Max-Age", "1728000");
+  return res.sendStatus(200);
+  next();
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.use('/api', movieRouter)
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.use("/api", movieRouter);
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
