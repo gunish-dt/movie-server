79b66c19ea3c67bfe2754fa1319fca4577637ffc
gunish-dt <gunish@devtron.ai>
2022-03-13 12:35:11 +0530

    changed mongo url

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectionString = "mongodb://mongo.gunish:27017/cinema";
+const connectionString =
+  "mongodb://mongo-gunish-env-mongodb.gunish:27017/cinema";
 
 mongoose.connect(connectionString, { useNewUrlParser: true }).catch((e) => {
   console.error("Connection error", e.message);
